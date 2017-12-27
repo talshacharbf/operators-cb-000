1eccac779ba72b57305aa4ed21392e876bfda2ab
tal shachar <tal.shachar@buzzfeed.com>
2017-12-27 06:40:13 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,17 @@
 def unsafe?(speed)
-
+  if speed > 60
+    true
+  elsif speed < 40
+    true
+  else false
+end
 end
 
 
 
 def not_safe?(speed)
-	
+  if speed > 60
+    true
+  else speed < 40 ? true:false
 end
-	
-
-
+end
